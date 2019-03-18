d179c8d007a3a501ef2df1001230b9d5a5962017
Leonardo Gutiérrez Hernández <leo342135@gmail.com>
2019-03-18 13:00:37 -0600

    Seleccion y deteccion de zona

--- src/components/auth/RegisterLogin.js
+++ src/components/auth/RegisterLogin.js
@@ -1,13 +1,77 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, ScrollView } from 'react-native';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
-const homePlace = { description: 'Home', geometry: { location: { lat: 48.8152937, lng: 2.4597668 } }};
-const workPlace = { description: 'Work', geometry: { location: { lat: 48.8496818, lng: 2.2940881 } }};
+//const homePlace = { description: 'Home', geometry: { location: { lat: 48.8152937, lng: 2.4597668 } }};
+//const workPlace = { description: 'Work', geometry: { location: { lat: 48.8496818, lng: 2.2940881 } }};
+import firebase from 'react-native-firebase';
 
 class RegisterLogin extends Component {
     static navigationOptions = {
         title: 'Establece una ubicación',
     };
+
+    constructor(props){
+        super(props);
+        this.state = {
+            selectedRegion: [],
+            selectedRoute: '',
+            selectedSublocality: '',
+            selectedPostalCode: '',
+            selectedCity: '',
+            selectedState: '',
+            selectedCountry: '',
+        };
+
+        this.generateId = this.generateId.bind(this);
+    }
+
+    generateId(){
+        //
+        firebase.auth().signInAnonymously()
+        .then(() => {
+            console.log('logged in')
+        });
+    }
+
+    processAddress(addr){
+        let route, sublocality, postalCode, city, state, country;
+        addr.forEach((addrPart)=>{
+            addrPart.types.forEach((addrPartType)=>{
+                switch (addrPartType) {
+                    case 'route':
+                        route = addrPart.long_name;
+                        break;
+                    case 'sublocality_level_1':
+                    case 'sublocality':
+                        sublocality = addrPart.long_name;
+                        break;
+                    case 'locality':
+                        city = addrPart.long_name;
+                        break;
+                    case 'administrative_area_level_1':
+                        state = addrPart.long_name;
+                        break;
+                    case 'country':
+                        country = addrPart.long_name;
+                        break;
+                    case 'postal_code':
+                        postalCode = addrPart.long_name;
+                        break;
+                    default:
+                        break;
+                }
+            });
+        });
+        console.log(route, sublocality, postalCode, city, state, country);
+        this.setState({
+            selectedRoute: route,
+            selectedSublocality: sublocality,
+            selectedPostalCode: postalCode,
+            selectedCity: city,
+            selectedState: state,
+            selectedCountry: country,
+        });
+    }
 
     render() {
         return (
@@ -22,7 +86,8 @@ class RegisterLogin extends Component {
                     fetchDetails={true}
                     renderDescription={row => row.description} // custom description render
                     onPress={(data, details = null) => { // 'details' is provided when fetchDetails = true
-                        console.log(data, details);
+                        console.log(details.address_components);
+                        this.processAddress(details.address_components);
                     }}
 
                     getDefaultValue={() => ''}
@@ -64,12 +129,13 @@ class RegisterLogin extends Component {
                     }}
 
                     filterReverseGeocodingByTypes={['locality', 'administrative_area_level_3']} // filter the reverse geocoding results by types - ['locality', 'administrative_area_level_3'] if you want to display only cities
-                    predefinedPlaces={[homePlace, workPlace]}
+                    //predefinedPlaces={[homePlace, workPlace]}
 
                     debounce={200} // debounce the requests in ms. Set to 0 to remove debounce. By default 0ms.
                     //renderLeftButton={()  => <Image source={require('path/custom/left-icon')} />}
                     //renderRightButton={() => <Text>Custom text after the input</Text>}
                     />
+                    
             </ScrollView>
         );
     }
