e803bcdfb8fd4c0e01552bc2db95737c2f72ae32
Leonardo Gutiérrez Hernández <leo342135@gmail.com>
2019-03-19 08:37:19 -0600

    Notificaciones push y configuraciones de release apk

--- src/components/neighborsAlarm/NeighborsAlarm.js
+++ src/components/neighborsAlarm/NeighborsAlarm.js
@@ -29,6 +29,7 @@ class NeighborsAlarm extends Component {
     }
 
     componentDidMount(){
+        this.initNotifications();
         // Enable playback in silence mode
         Sound.setCategory('Playback');
 
@@ -87,6 +88,83 @@ class NeighborsAlarm extends Component {
             this.alarmSubscriber();
         }
         this.alarmSound.release();
+    }
+
+    initNotifications(){
+        firebase.messaging().hasPermission()
+        .then(enabled => {
+            if (enabled) {
+                // user has permissions
+                console.log(enabled)
+                this.listenToNotifications();
+            } else {
+                console.log('user doesnt have permission')
+                // user doesn't have permission
+                firebase.messaging().requestPermission()
+                .then(() => {
+                    // User has authorised  
+                    this.listenToNotifications();
+                })
+                .catch(error => {
+                    // User has rejected permissions 
+                    console.error(error); 
+                });
+            }
+        });
+    }
+
+    listenToNotifications(){
+        const channel = new firebase.notifications.Android.Channel('alarm-channel', 'Canal de alarmas', firebase.notifications.Android.Importance.Max)
+          .setDescription('Medio por donde se reciben alertas de seguridad');
+    
+        // Create the channel
+        firebase.notifications().android.createChannel(channel);
+    
+        firebase.messaging().getToken()
+        .then(fcmToken => {
+          console.log(fcmToken)
+          if (fcmToken) {
+            // user has a device token
+            
+          } else {
+            // user doesn't have a device token yet
+          } 
+        });
+        this.messageListener = firebase.messaging().onMessage((message) => {
+          // Process your message as required
+          console.log(message,"YES");
+        });
+        this.notificationDisplayedListener = firebase.notifications().onNotificationDisplayed((notification) => {
+            // Process your notification as required
+            // ANDROID: Remote notifications do not contain the channel ID. You will have to specify this manually if you'd like to re-display the notification.
+            console.log(notification);
+        });
+        this.notificationListener = firebase.notifications().onNotification((notification) => {
+            // Process your notification as required
+            console.log(notification);
+        });
+        this.notificationOpenedListener = firebase.notifications().onNotificationOpened((notificationOpen) => {
+          // Get the action triggered by the notification being opened
+          const action = notificationOpen.action;
+          // Get information about the notification that was opened
+          const notification = notificationOpen.notification;
+          console.log(action, notification)
+        });
+        firebase.notifications().getInitialNotification()
+          .then((notificationOpen) => {
+            if (notificationOpen) {
+              // App was opened by a notification
+              // Get the action triggered by the notification being opened
+              const action = notificationOpen.action;
+              // Get information about the notification that was opened
+              const notification = notificationOpen.notification;  
+              console.log(action, notification)
+            }
+          });
+          this.onTokenRefreshListener = firebase.messaging().onTokenRefresh(fcmToken => {
+              // Process your token as required
+              console.log(fcmToken);
+        });
     }
     
     toggleAlert(){
